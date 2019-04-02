ac60b0c215c82312ccec9bcf04e1e49b80d818ea
zhangxiaoqiang <1301807538@qq.com>
2019-04-02 16:27:02 +0800

    组装件传值

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Com from './componments/com'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      parentText: "现在是父组件",
+      arr: [{
+          "userName": "fangMing", "text": "123333", "result": true
+      }, {
+          "userName": "zhangSan", "text": "345555", "result": false
+      }, {
+          "userName": "liSi", "text": "567777", "result": true
+      }, {
+          "userName": "wangWu", "text": "789999", "result": true
+      },]
+    };
+    this.foo = "我来自父组件"
+  };
+  fn(data) {
+    this.setState({
+        parentText: data //把父组件中的parentText替换为子组件传递的值
+    },() =>{
+       console.log(this.state.parentText);//setState是异步操作，但是我们可以在它的回调函数里面进行操作
+    });
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="logo" />                    
         </header>
+        <Com age="大卫" arr={this.state.arr} fn={this.foo} pfn={this.fn.bind(this)}/> {/*通过绑定事件进行值的运算，这个地方一定要记得.bind(this)，不然会报错，切记切记，因为通过事件传递的时候this的指向已经改变 */}
+        <p>text is {this.state.parentText}</p>
       </div>
     );
   }
---
+++ src/componments/com.jsx
@@ -0,0 +1,37 @@
+﻿import React, { Component } from 'react';
+
+class Ele extends Component{
+    constructor(props){
+       super(props);
+       this.state = ({
+            childText: "我来自子组件"
+       })  
+    };
+    clickFun(text) {  //定义触发的方法
+        this.props.pfn(text)//这个地方把值传递给了props的事件当中
+    }
+    render(){
+        return (
+           <div>
+               <h1>Hello, {this.props.age}</h1>
+               <p>{this.props.fn}</p>
+               <ul>
+                    {
+                        this.props.arr.map(item => { //这个地方通过this.props.arr接收到父组件传过来的arr，然后在{}里面进行js的循环
+                            return (
+                                <li key={item.userName}>
+                                    {item.userName} 评论是:{item.text}
+                                </li>
+                            )
+                        })
+                    }
+               </ul>
+                {/* 通过事件进行传值，如果想得到event，可以在参数最后加一个event，
+                这个地方还是要强调，this，this，this */}
+                <button onClick={this.clickFun.bind(this, this.state.childText)}>传值</button>
+           </div>
+        );
+    };
+}
+
+export default Ele;
